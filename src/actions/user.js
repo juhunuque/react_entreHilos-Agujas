162635a6f98ceec2c498c994ca5bc86c1f5fd66c
Julio N <juhunuque@gmail.com>
2016-11-23 08:43:49 -0600

    Revert "make it better"
    This reverts commit 981794f67d86b3343d1c4242f1b749f1b8edb6e9.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// export const SERVER_URL = 'http://localhost:3000/';
-export const SERVER_URL = 'http://ec2-35-160-181-181.us-west-2.compute.amazonaws.com:3000/';
+export const SERVER_URL = 'http://localhost:3000/';
 
 export const FETCH_USERS = 'FETCH_USERS';
 export const LOGIN = 'LOGIN';
